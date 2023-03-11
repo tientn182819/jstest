c4f3fc9599482dbebca78260c6a6791a9e1a07ba
tientn182819 <79956719+tientn182819@users.noreply.github.com>
2023-03-11 15:18:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -71,6 +71,15 @@
 //SAU KHI DÙNG TIỀN TỐ THÌ GIÁ TRỊ CỦA a LÀ 2, VỚI HẬU TỐ ĐẰNG SAU THÌ VẪN SẼ SỬ DỤNG a = 2 VÌ SỬ DỤNG HẬU TỐ
 // VẬY CONSOLE SẼ RA KẾT QUẢ LÀ 4
 
+
+
+
+
+
+
+
+
+
 //--------------------TOAN TU CHUOI ----------------------\\
 
 // var firstName = 'Tien'
@@ -88,6 +97,11 @@
 // name += ' Do' // name = name + 'Do';
 
 // console.log(name)
+
+
+
+
+
 
 //-----------------TOAN TU SO SANH----------------------\\
 
@@ -116,11 +130,21 @@
 
 //Toán tử logic và câu điều kiện if
 
+
+
+
+
+
 //------------KIEU DU LIEU BOOLEAN--------------------\\
 
 //  var isSuccess = true; //false
 
 //  console.log(isSuccess);
+
+
+
+
+
 
  //---------TOAN TU LOGIC------------------\\
 
@@ -138,6 +162,16 @@
 //  } else {
 //     console.log('Sai')
 //  }
+
+
+
+
+
+
+
+
+
+
 
  //----------KIỂU DỮ LIỆU----------\\
 
@@ -200,6 +234,26 @@
 // // Check kiểu dữ liệu
 // console.log(typeof a);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //----------------TRUTHY VÀ FALSY---------------
 
 /** Bất cứ giá trị nào trong Javascript khi chuyển đổi sang kiểu dữ liệu boolean mà có giá trị true thì ta gọi giá trị đó là Truthy */
@@ -233,3 +287,181 @@
 // Khi typeof document.all sẽ trả về "undefined" */
 
 // console.log(document.all)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //------------------THAM SỐ ----------------------
+// function writeLog(log, log2){ //log là một tham số 
+//   console.log(log, log2)
+// }
+
+// writeLog('TEST', 'TEST2');// đây là đối số 
+// //Kiểu dữ liệu: không giới hạn
+// //Tính Private - một biến chỉ được dùng trong hàm đó. (writeLog)
+//có thể truyền một hoặc nhiều biến 
+
+//Argument dùng để thay thế cho việc liệt kê quá nhiêu ftham số trogn hàm: VD:
+
+// function writeLog2(Log){
+//   console.log(arguments)
+// }
+
+// writeLog2(1, 2, 3) //Nêus bth muốn in cái này thì phải truyền 3 tham số trong writelog nhưng không cần 
+
+
+
+
+
+
+
+
+
+
+
+//-------------------- VÒNG FOR VƠIS THAM SỐ 
+
+// function writeLogFor(){
+//   for (var param of arguments){
+//     console.log(param)
+//     /*Param là biết 
+//     khi for chạy giá trị đầu tiên sẽ lấy giá trị của mảng arguments gán vào param, sau đó in mỗi dòng 1 thằng.
+//     Số lần chạy bằng số giá trị trong mảng arguments, chính là trong hàm khai báo ở dưới  */
+//   }
+// }
+// writeLogFor(1, 2, 3)
+
+// /*Nối thêm giá trị bằng một ký tự khác*/
+
+// function writeForLog2(){
+//   var myString ='';
+//   for (var param2 of arguments){
+//     myString += `${param2} - ` //template String
+//   }
+//   console.log(myString)
+// }
+
+// writeForLog2(1, 2, 3, 4, 5, 6, 7);
+
+
+
+
+
+//-----------------TỪ KHÓA RETURN TRONG HÀM --------------------
+// var isConfirm = confirm('Mesage'); 
+// //Khi sử dụng hàm này thì những hàm sau nó sẽ k được hiển thị 
+
+// console.log(isConfirm)
+
+// function cong(a,b){
+//   return a + b; // return sẽ báo hàm trả về giá trị a+b, nếu không khai báo return thì sẽ trả về giá trị undefined 
+//   //nếu có dòng code dưới return thì chúng sẽ khong chạy  
+// }
+
+// var result = cong(2,8)
+// console.log(result)
+
+// //Những hàm mà nổ pop up sau khi chạy thì sẽ không thực thi code sau đó 
+
+// function cong2(c, d){
+//   return c.toString() + d.toString(); // a.toString sẽ biến a thanfhc huỗi chữ không phải số
+// }
+
+// var result2 = cong2(2,8)
+// console.log(result2)
+
+
+
+
+//-----------------------HIEU THÊM VỀ FUNCTION----------------
+//Nếu 2 function trùng nhau thì nó sẽ thực hiện lệnh của function mới nhất 
+
+// function showMessage(){
+//   console.log('show message 1');
+// }
+
+// function showMessage(){
+//   console.log('show message 2');
+// }
+
+// showMessage(); // kết quả trả về là show message 2 
+
+// //Có thể khai báo được biến ở trong hàm 
+//  function showMessage2(){
+//   var name = 'ABC'
+//   console.log(name)
+//  }
+
+//  showMessage2(); //name chỉ dược định nghĩa và sử dụng trong function
+
+
+
+ 
+
+ //---------------------CÁC LOẠI FUNCTION-------------------------
+//  showMessage();
+
+//  function showMessage(){
+//   console.log('Declareation Function')
+//  } // đây là Declaration Function 
+
+// //Với decla function, hàm có thể được gọi trước khi nó được định nghĩa
+
+//  var showMessage2 = function(){
+//   console.log('Expression Function')
+//  }; // đây là Expression Function 
+
+//  showMessage2();
+//  //Expre Function bắt buộc phải gọi sau khi định nghĩa hàm
+//  setTimeout(function autoLogin(){ //autoLogin chỉ là cái tên 
+
+//  });
+
+//  var myObject = {
+//   myFunction: function(){}
+//  }
+
+
+
+
+//-----------------------LÀM VIỆC VỚI CHUỖI----------------------
+// Có hai cách tạo chuỗi 
+//c1: 
+var fullName = 'Tien'
+
+//c2
+var fullName2 = new String('Tien')// không dùng vì kiểu dữ liệu là object
+
+//Case sử dụng dấu \
+var fullName3 = 'Tran Ngoc \'Tien\''
+console.log(fullName3)
+//JS không phân biệt được 2 dấu nhasy đơn khi để vào trong string vì vậy dùng dáu \ để đánh dấu dấu nháy đơn trong String
+
+var backSlash = ' Day la dau gach cheo nguoc \\'
+//Khi muốn hiển thị dấu gạch chéo ngược thì mình phải dùng 2 dấu 
+
+//Xem độ dài chuỗi 
+console.log(fullName.length)
+console.log(fullName)
+
+//Template String ES6
+var name1 = 'Tran'
+var name2 = 'Tien'
+console.log(`Toi la: ${name1} ${name2} `)
+
+
+
+
+//--------------------
+
